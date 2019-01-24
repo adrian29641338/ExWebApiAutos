836ce30455dbbc378a0ba94bf0367ac892cbce7a
Adrián Wilder Fuentes Zuñiga <tefu-sac@hotmail.com>
2019-01-24 11:13:12 -0500

    Generación Entidades
    Generé entidades a partir de base de datos, usando EF.

---
+++ ExWebApiAutos/ExWebApiAutos/Model/ExWebApiAutos/ExWebApiAutosDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ExWebApiAutos.Model.ExWebApiAutos
+{
+    public partial class ExWebApiAutosDbContext : DbContext
+    {
+        public ExWebApiAutosDbContext()
+        {
+        }
+
+        public ExWebApiAutosDbContext(DbContextOptions<ExWebApiAutosDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TAuto> TAuto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("data source = LAPTOP-RL3DO4FV; initial catalog = ExamenAutos; Integrated Security = true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TAuto>(entity =>
+            {
+                entity.Property(e => e.AutoId).ValueGeneratedNever();
+
+                entity.Property(e => e.AutoColor).IsUnicode(false);
+
+                entity.Property(e => e.AutoFull).IsUnicode(false);
+
+                entity.Property(e => e.AutoMarca).IsUnicode(false);
+
+                entity.Property(e => e.AutoMecanico).IsUnicode(false);
+
+                entity.Property(e => e.AutoNroplaca).IsUnicode(false);
+            });
+        }
+    }
+}
---
+++ ExWebApiAutos/ExWebApiAutos/Model/ExWebApiAutos/TAuto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExWebApiAutos.Model.ExWebApiAutos
+{
+    [Table("T_Auto")]
+    public partial class TAuto
+    {
+        [Key]
+        [Column("auto_id")]
+        public Guid AutoId { get; set; }
+        [Required]
+        [Column("auto_color")]
+        [StringLength(20)]
+        public string AutoColor { get; set; }
+        [Column("auto_aniofabri")]
+        public int AutoAniofabri { get; set; }
+        [Required]
+        [Column("auto_nroplaca")]
+        [StringLength(10)]
+        public string AutoNroplaca { get; set; }
+        [Column("auto_nroasientos")]
+        public int AutoNroasientos { get; set; }
+        [Required]
+        [Column("auto_mecanico")]
+        [StringLength(2)]
+        public string AutoMecanico { get; set; }
+        [Required]
+        [Column("auto_full")]
+        [StringLength(2)]
+        public string AutoFull { get; set; }
+        [Required]
+        [Column("auto_marca")]
+        [StringLength(15)]
+        public string AutoMarca { get; set; }
+    }
+}
